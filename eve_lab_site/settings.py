f0fe48094436db97c2c254c757c223b572dd710e
root <root@ip-172-31-15-7.ap-south-1.compute.internal>
2025-06-13 13:43:33 +0000

    Enhanced Upload EVE-Ng Lab File template with improved styling

--- eve_lab_site/settings.py
+++ eve_lab_site/settings.py
@@ -15,10 +15,11 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static & Media files config
-STATIC_URL = '/static/'
+
+STATIC_URL = '/staticfiles/admin/img'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
